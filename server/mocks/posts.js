3f9fa864ec9bb435cc1a194a557e8c73eaaa3af6
hbaughman <hbaughman741@gmail.com>
2015-02-24 18:22:18 -0500

    Create mocks for posts

---
+++ server/mocks/posts.js
@@ -0,0 +1,39 @@
+module.exports = function(app) {
+  var express = require('express');
+  var postsRouter = express.Router();
+
+  postsRouter.get('/', function(req, res) {
+    res.send({
+      'posts': [
+        { id: 1, title: "foo" },
+        { id: 2, title: "bar" }
+      ]
+    });
+  });
+
+  postsRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  postsRouter.get('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.put('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/posts', postsRouter);
+};
