61d6cea1eecb2093f146eaa04156ced29f35b0df
hbaughman <hbaughman741@gmail.com>
2015-02-24 20:48:52 -0500

    Add Array.find polyfill snippet

--- server/mocks/posts.js
+++ server/mocks/posts.js
@@ -1,3 +1,28 @@
+// Array.find polyfill
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined.');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this),
+        length = list.length >>> 0,
+        thisArg = arguments[1],
+        value;
+
+    for (var ii = 0; ii < length; ii++) {
+      value = list[ii];
+      if (predicate.call(thisArg, value, ii, list)) {
+        return value;
+      }
+    }
+    return undefined
+  };
+}
+
 module.exports = function(app) {
   var express = require('express');
   var postsRouter = express.Router();
