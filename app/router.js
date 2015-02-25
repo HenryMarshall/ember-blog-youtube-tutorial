62be8bc6aa2b55b75175b4bab6077feaf1edcb39
hbaughman <hbaughman741@gmail.com>
2015-02-24 21:04:18 -0500

    Nest post within posts

--- app/router.js
+++ app/router.js
@@ -7,8 +7,9 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
-  this.resource('posts');
-  this.resource('post', { path: '/posts/:post_id' });
+  this.resource('posts', function() {
+    this.resource('post', { path: ':post_id' });
+  });
 });
 
 export default Router;
